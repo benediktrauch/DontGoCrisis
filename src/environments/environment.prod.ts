462b099945380ffb3fbcd29b52bb7350d1c1d6bd
BenediktRauch <benedikt.rauch@stud.hs-ruhrwest.de>
2020-03-22 10:43:40 +0100

    configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'XXX',
+    authDomain: 'krise-als-chance.firebaseapp.com',
+    databaseURL: 'https://krise-als-chance.firebaseio.com',
+    projectId: 'krise-als-chance',
+    storageBucket: 'krise-als-chance.appspot.com',
+    messagingSenderId: '0000000',
+    appId: 'XXXXXXX'
+  }
+};
